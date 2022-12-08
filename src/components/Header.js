a6438859a15943412b59ac3d1a27ea7d88a3872b
danieleugenevoigt <dvoigt@tutanota.com>
2022-12-08 11:30:17 -0700

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import classes from './Header.module.css';
+
+const Header = () => {
+  return (
+    <div >
+      <nav className={classes.image}>
+        <h1>Performance Racing Composites</h1>
+        <ul>
+          <link href="" />
+        </ul>
+      </nav>
+    </div>
+  )
+}
+
+export default Header
