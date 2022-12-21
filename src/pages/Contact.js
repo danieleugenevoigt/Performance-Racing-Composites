eb5db032efef6f06fd2d6474d3b2d466fff1898a
Daniel Voigt <dvoigt@tutanota.com>
2022-12-20 19:48:17 -0700

    Added Product button for database entry

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,17 +1,24 @@
 import React from 'react'
 import ContactForm from '../components/ContactForm';
 import classes from './Contact.module.css';
-
+import ProductForm from '../components/ProductForm';
 import { useState } from 'react';
 
 const Contact = () => {
 
   const [isShow, setIsShow] = useState(true);
+  const [isShow2, setIsShow2] = useState(true);
   
 
   
     function toggle(e) {
       setIsShow(!isShow);
+      console.log( e.target.id);
+      
+    }
+
+    function toggle2(e) {
+      setIsShow2(!isShow2);
       console.log( e.target.id);
       
     }
@@ -27,11 +34,12 @@ const Contact = () => {
         prompt service.
       </p>
       <button id={1} onClick={toggle}>Contact Us!</button> 
+      <button id={2} onClick={toggle2}>Add Product</button>
       
       <div className={classes.container}>
       
-        <ContactForm isShow={isShow} dept1={'sales@prc.com'} dept2={'support@prc.com'} className={classes.contactForm} />
-        
+        <ContactForm isShow={isShow} className={classes.contactForm} />
+        <ProductForm isShow2={isShow2}className={classes.ProductForm}/>
       </div>
     </div>
   );
