962be3f8836207f44e492c90ae653dbc667b4d13
Daniel Voigt <dvoigt@catalyte.io>
2023-08-11 08:01:44 -0600

    Logo.svg added/not working

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import classes from './Header.module.css';
-
 import { Link } from 'react-router-dom';
+import { PRCLogo2 } from '../images/PRCLogo2.svg';
 
 const Header = () => {
   return (
@@ -9,7 +9,12 @@ const Header = () => {
         <h1>Performance Racing Composites</h1>
         <ul>
         <li>
-         <Link className={classes.link} to='/home'>Home</Link>
+         <Link className={classes.link} to='/home'>
+         <svg width="100" height="100" viewBox="0 0 100 100">
+          {PRCLogo2}
+          <circle cx="50" cy="50" r="40" stroke="black" strokeWidth="3" fill="red" />
+        </svg>
+         </Link>
          </li>
          <li>
          <Link className={classes.link}to='/products'>Products</Link>
