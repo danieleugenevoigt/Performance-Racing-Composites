8656ddbb8427d7c05b6854d818a3982901dd97ac
Daniel Voigt <dvoigt@catalyte.io>
2023-08-16 17:09:30 -0600

    initial useEffect created.Not working

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,9 +1,28 @@
+import { useEffect } from 'react';
 import Road1 from '../images/Road1.mp4';
 import classes from './About.module.css';
 const About = () => {
+
+    useEffect(() => {
+        const video = document.getElementById('background-video');
+        const overlay = document.querySelector('.overlay');
+
+        const handleVideoEnd = () => {
+            overlay.style.backgroundColor = '#4E4D4A'; // Replace with your desired color
+            overlay.style.opacity = '1';
+        };
+
+        video.addEventListener('ended', handleVideoEnd);
+
+        return () => {
+            video.removeEventListener('ended', handleVideoEnd);
+        };
+    }, []);
+
   return (
     <div className={classes.main}>
-      <video className={classes.video} src={Road1} autoPlay muted></video>
+      <video id={"background-video"} className={classes.video} src={Road1} autoPlay muted></video>
+      <div className={classes.overlay}></div>
 
       <section className={classes.textSection}>
         <h1>Racing Is In Our Blood</h1>
