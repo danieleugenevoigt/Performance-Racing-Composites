e79b89e63170c3d6976637c6b47eb9d52cce8edc
daniel <daniel@pop-os.localdomain>
2023-08-25 13:16:57 -0600

    started toggle function for vehicleselector

--- src/components/VehicleSelector/VehicleSelector.js
+++ src/components/VehicleSelector/VehicleSelector.js
@@ -1,12 +1,23 @@
-import React from 'react';
+import { React, useState } from 'react';
 import classes from '../VehicleSelector/VehicleSelector.module.css';
 
 const VehicleSelector = () => {
+
+  const [showVehicleSelector, setShowVehicleSelector] = useState(true);
+
+  const toggleSelector = () => {
+    setShowVehicleSelector(!showVehicleSelector);
+  }
+
   return (
     <div className={classes.container}>
       <div className={classes.content}>
         <h2 className={classes.search}>Vehicle Search</h2>
-        <div className={classes.close}><span className="material-symbols-outlined">close</span></div>
+        <div className={classes.close}>
+          {showVehicleSelector &&
+            <span className="material-symbols-outlined" onClick={toggleSelector}>close</span>
+          }
+        </div>
       </div>
     </div>
   )
